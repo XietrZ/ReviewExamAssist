a6be6441768428dad3f8adc3d786f28bd1dfc968
XietrZ <sxietrz@yahoo.com>
2023-06-19 23:26:17 +0800

    Initialize

---
+++ App/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/style-prop-object */
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Revew Exam Assist</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
