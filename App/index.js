a1ac0eee6b2963bde483fbd6193c3d4a7288c9d4
XietrZ <sxietrz@yahoo.com>
2023-06-20 18:15:32 +0800

    Make running in web work

--- App/index.js
+++ App/index.js
@@ -3,11 +3,12 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Revew Exam Assist</Text>
+      <Text>Revew Exam Assist asasas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+registerRootComponent(App);
